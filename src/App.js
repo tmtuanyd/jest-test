533288cb9568d3ef00c3580d3e107eb6e891f284
tmtuan <mtr@silico.vn>
2020-11-20 17:16:22 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./component/header/Header";
 import './app.scss'
+import HeadLine from "./component/headline/HeadLine";
 
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <section className={"main"}>
+            <HeadLine header={"Post"} desc={"Click the button to render posts!"}/>
+        </section>
     </div>
   );
 }
--- src/component/header/spec.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {shallow} from 'enzyme'
-import Header from "./Header";
-
-it('It should render without errors', ()=>{
-    expect(1).toBe(3)
-})
